91f5334ba4e10328b23999c9e7a048e9d246358b
jvmantovas <vmsjoao@gmail.com>
2022-09-26 22:06:08 -0300

    JS Basics - Object Challenge

--- Challanges/Section 3 - JS Basics/06 - Objects/app.js
+++ Challanges/Section 3 - JS Basics/06 - Objects/app.js
@@ -1,20 +1,18 @@
-function calculateTotal(subTotal, tax) {
-  const total = subTotal + tax;
-  return total;
-}
-
-console.log(calculateTotal(200, 20));
-
-const order1 = calculateTotal(100, 10);
-const order2 = calculateTotal(50, 5);
-const order3 = calculateTotal(25, 5);
-
-console.log(order1, order2, order3);
-
-/*refactoring to function expression:*/
-const calculateTotalRefactored = function (subTotal, tax) {
-  const total = subTotal + tax;
-  return total;
+const car = {
+  make: "Chevrolet",
+  model: "Monza",
+  year: 1997,
+  colors: ["white", "red", "grey", "black"],
+  hybrid: false,
+  drive: function () {
+    console.log("You started driving the car.");
+  },
+  stop() {
+    console.log("You stopped the car.");
+  },
 };
 
-console.log(calculateTotalRefactored(200, 20));
+console.log(car.make);
+console.log(car.colors[0]);
+car.drive();
+car.stop();
