f612bd965a1a5908201c113b3e9943bd1a7f8815
jvmantovas <vmsjoao@gmail.com>
2022-10-17 17:59:01 -0300

    S5 88-92

---
+++ Exs/ex38 - DOM window and document overview/app.js
@@ -0,0 +1,7 @@
+// window object = browser api
+// document
+// console.dir
+
+console.log(window);
+
+console.dir(document); //directions of the object (properties and methods)
---
+++ Exs/ex39 - Get Element By ID/app.js
@@ -0,0 +1,10 @@
+// Select the element or group of elements that we want
+// Decide the effect we want to apply to the selection
+
+// getElementById('element')
+
+const title = document.getElementById("title");
+title.style.color = "red"; //always better to assign to a variable, so you can control it easier
+title.style.backgroundColor = "blue";
+
+document.getElementById("text").style.backgroundColor = "lightgrey"; //hardest way
