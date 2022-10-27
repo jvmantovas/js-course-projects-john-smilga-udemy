b00fca0601812fe17dee4097a9950de8b244c842
jvmantovas <vmsjoao@gmail.com>
2022-10-27 19:10:45 -0300

    S6 147-150 done

--- Projects/07-questions/setup/app.js
+++ Projects/07-questions/setup/app.js
@@ -1,2 +1,26 @@
-//using selectors inside the element
 // traversing the dom
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+//using selectors inside the element - best
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
